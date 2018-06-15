4160e1d59fe93c782e00645e03726a20533da66c
Bobby Yang <bobby.yang@makewonder.com>
2018-06-14 19:51:19 -0700

    invert method with 2048

--- 0x04-slide_line/0-slide_line.c
+++ 0x04-slide_line/0-slide_line.c
@@ -4,194 +4,67 @@
 
 #include "slide_line.h"
 
-/**
- * squish_line_left - squishes line left
- * @line: line
- * @size: size
- * Return: number of non-zero numbers
- */
-int squish_line_left(int **line, size_t size)
-{
-	size_t position;
-	int *hold;
-	int *move;
-	int total_numbers;
-
-	total_numbers = 0;
-	position = 0;
-	hold = *line;
-	move = *line;
-
-	while (position < size)
-	{
-		if (*move)
-		{
-			*hold = *move;
-			hold++;
-			total_numbers++;
-		}
-
-		move++;
-		position++;
-	}
-
-	return (total_numbers);
-}
-
-/**
- * squish_line_right - squishes line right
- * @line: line
- * @size: size
- * Return: number of non-zero numbers
- */
-int squish_line_right(int **line, size_t size)
-{
-	size_t position;
-	int *hold;
-	int *move;
-	int total_numbers;
-
-	total_numbers = 0;
-	position = size - 1;
-	hold = *line + size - 1;
-	move = *line + size - 1;
-
-	while (position < size)
-	{
-		if (*move)
-		{
-			*hold = *move;
-			hold--;
-			total_numbers++;
-		}
-
-		move--;
-		position--;
-	}
-
-	return (total_numbers);
-}
 
 /**
  * slide_line_left - slides left
  * @line: line
  * @size: size of line
- * @number_length: numbers in line
- * Return: 1 for success 0 for fail
  */
-int slide_line_left(int *line, size_t size, size_t number_length)
+void slide_line_left(int *line, size_t size)
 {
-	int *position = line;
-	int *current = line;
-	int *next = line;
-	size_t next_track = 0;
-	size_t p = 0;
+	int *a = line;
+	size_t i;
+	size_t pos = 0;
 
-	while (p < size && next_track < size)
+	for (i = 1; i < size; i++)
 	{
-		next++;
-		next_track++;
-
-		if (p >= number_length - 1 && number_length != size)
+		if (a[i] != 0)
 		{
-			line[p] = 0;
-			p++;
-			continue;
+			if (a[pos] == 0)
+			{
+				a[pos] = a[i];
+				if (i != pos)
+					a[i] = 0;
+			}
+			else if (a[i] == a[pos])
+			{
+				if (pos != i)
+				{
+					a[pos] += a[i];
+					a[i] = 0;
+					pos++;
+				}
+			}
+			else
+			{
+				pos++;
+				if (i != pos)
+				{
+					a[pos] = a[i];
+					a[i] = 0;
+				}
+			}
 		}
-
-		if (*current == 0)
-		{
-			*current = *next;
-			*next = 0;
-			next++;
-			next_track++;
-		}
-
-		if (*current == *next)
-		{
-			*position = *current + *next;
-			*next = 0;
-		}
-		p++;
-		position++;
-		current++;
 	}
-
-	while (p < size)
-	{
-		if (p >= number_length - 1 && number_length != size)
-		{
-			line[p] = 0;
-			p++;
-			continue;
-		}
-		p++;
-	}
-
-	return (1);
-
 }
 
 /**
- * slide_line_right - slides right
- * @line: line
- * @size: size of line
- * @number_length: numbers in line
- * Return: 1 for success 0 for fail
- */
-int slide_line_right(int *line, size_t size, size_t number_length)
+* invert - invert the elements of the line
+* @line: line
+* @size: size of line
+*/
+void invert(int *line, size_t size)
 {
-	int *position = line + size - 1;
-	int *current = line + size - 1;
-	int *next = line + size - 1;
-	size_t next_track = 0;
-	size_t p = size - 1;
+	size_t i;
+	int aux;
+	int *a = line;
 
-	while ((int)p >= 0 && next_track < size)
+	for (i = 0; i < size / 2; i++)
 	{
-		next--;
-		next_track++;
-
-		if (p <= size - number_length && number_length != size)
-		{
-			line[p] = 0;
-			p--;
-			continue;
-		}
-
-		if (*current == 0)
-		{
-			*current = *next;
-			*next = 0;
-			next--;
-			next_track++;
-		}
-
-		if (*current == *next)
-		{
-
-			*position = *current + *next;
-			*next = 0;
-		}
-
-		p--;
-		position--;
-		current--;
+		aux = a[i];
+		a[i] = a[size - 1 - i];
+		a[size - 1 - i] = aux;
 	}
-
-	while ((int)p >= 0)
-	{
-		if (p <= size - number_length && number_length != size)
-		{
-			line[p] = 0;
-			p--;
-			continue;
-		}
-		p--;
-	}
-
-	return (1);
-
 }
 
 /**
@@ -203,20 +76,18 @@ int slide_line_right(int *line, size_t size, size_t number_length)
  */
 int slide_line(int *line, size_t size, int direction)
 {
-	size_t number_length = 0;
-
-	if (line == NULL)
-	{
+	if (line == NULL
+		|| size <= 0
+		|| (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
 		return (0);
-	}
 
 	if (direction == SLIDE_LEFT)
+		slide_line_left(line, size);
+	else
 	{
-		number_length = squish_line_left(&line, size);
-		return (slide_line_left(line, size, number_length));
+		invert(line, size);
+		slide_line_left(line, size);
+		invert(line, size);
 	}
-
-	number_length = squish_line_right(&line, size);
-
-	return (slide_line_right(line, size, number_length));
+	return (1);
 }
--- 0x04-slide_line/slide_line.h
+++ 0x04-slide_line/slide_line.h
@@ -1,13 +1,11 @@
-#ifndef INC_0X04_SLIDE_LINE_SLIDE_LINE_H
-#define INC_0X04_SLIDE_LINE_SLIDE_LINE_H
+#ifndef _SLIDE_LINE_H
+#define _SLIDE_LINE_H
 
 #define SLIDE_LEFT 1
 #define SLIDE_RIGHT 2
 
-int squish_line_left(int **line, size_t size);
-int squish_line_right(int **line, size_t size);
-int slide_line_left(int *line, size_t size, size_t number_length);
-int slide_line_right(int *line, size_t size, size_t number_length);
+void slide_line_left(int *line, size_t size);
 int slide_line(int *line, size_t size, int direction);
+void invert(int *line, size_t size);
 
 #endif
