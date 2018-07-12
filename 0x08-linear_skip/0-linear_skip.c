f9d0fa579ec915341d811ae56c3f64af7515da10
glyif <122@holbertonschool.com>
2018-07-12 11:24:11 -0700

    0x08 revised linear skip search

--- 0x08-linear_skip/0-linear_skip.c
+++ 0x08-linear_skip/0-linear_skip.c
@@ -4,6 +4,25 @@
 #include "search.h"
 
 /**
+ * linear_search - linear search through linked list
+ * @first: first node
+ * @last: last node
+ * @value: value to search for
+ * Return: found node or null;
+ */
+skiplist_t *linear_search(skiplist_t *first, skiplist_t *last, int value)
+{
+	while (first && first->index <= last->index)
+	{
+		printf("Value checked at index [%d] = [%d]\n", (int) first->index, first->n);
+		if (first->n == value)
+			return first;
+		first = first->next;
+	}
+	return NULL;
+}
+
+/**
  * linear_skip - linear search using skip list
  * @list: head of skip list
  * @value: value to search for
@@ -12,10 +31,16 @@
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *slow;
+	skiplist_t *temp;
 	skiplist_t *fast;
+	int last_index;
+	int count;
 
-	slow = list;
-	fast = slow->express;
+	temp = list;
+	slow = temp->express;
+	fast = temp->express;
+	count = 0;
+	last_index = 0;
 
 	if (list == NULL)
 		return (NULL);
@@ -24,23 +49,35 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	{
 		printf("Value checked at index [%d] = [%d]\n", (int) fast->index, fast->n);
 
-		if (value <= fast->n && value >= slow->n)
+		if ((value <= fast->n && value >= slow->n) || (count == 0 && value <= fast->n))
 		{
+			if (count == 0) {
+				slow = temp;
+			}
+
 			printf("Value found between indexes [%d] and [%d]\n",
 				   (int) slow->index, (int) fast->index);
 
-			while (slow->index <= fast->index)
-			{
-				printf("Value checked at index [%d] = [%d]\n", (int) slow->index, slow->n);
-				if (slow->n == value)
-					return (slow);
-				slow = slow->next;
-			}
+			return linear_search(slow, fast, value);
 		}
 
-		slow = slow->next;
+		count++;
+
+		slow = fast;
 		fast = fast->express;
 	}
 
-	return (NULL);
+	temp = slow;
+	last_index = slow->index;
+
+	while(temp->next)
+	{
+		last_index++;
+		temp = temp->next;
+	}
+
+	printf("Value found between indexes [%d] and [%d]\n",
+		   (int) slow->index, (int) temp->index);
+
+	return (linear_search(slow, temp, value));
 }
